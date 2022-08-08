b56c5804e69c4d1d234534b87c253f4b8c52940d
Harish B <harishbunicorn@gmail.com>
2022-08-08 09:19:41 +0530

    first change

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello harish welcome to nest and enjoy your journey';
   }
 }
