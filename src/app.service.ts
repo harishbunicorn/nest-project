5340754f450a41b3a4d4cf76d89decb2f63aaf2d
Harish B <harishbunicorn@gmail.com>
2022-08-18 20:32:45 +0530

    initial auith service setup

--- src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello harish welcome to nest and enjoy your journey';
-  }
-}
